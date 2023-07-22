34d4cddaa826dffe049335946f7398b3d9f26d51
kevinduhamelhayes <kevinduhamelh@gmail.com>
2023-07-22 17:21:01 -0300

    merge

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import "./styles/body.css"
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <div className="body-container">
+      <div className="body-title">
+        <h1 className="title-body">Welcome to my portfolio</h1>
+        <h1 className="title-body-gradient"> </h1>
+        <p className="body-p">
+          Meet me, a skilled and dynamic programmer, fully equipped to conquer
+          new challenges in the realm of software development. My passion for
+          cutting-edge technologies, coupled with a proven track record of
+          delivering exceptional results, ensures that I bring the perfect blend
+          of creativity and precision to every project I undertake.
+        </p>
+
+        <ul className="ul-body">
+          What sets me apart?
+          <li className="li-body">
+            🌟 Proficiency in multiple programming languages, ranging from
+            Python and JavaScript to CSS and beyond, enables me to thrive in
+            diverse tech landscapes.
+          </li>
+          <li className="li-body">
+            🌟 I continuously refine my skills, keeping abreast of the latest
+            industry trends, making me a valuable asset for your team.
+          </li>
+          <li className="li-body">
+            🌟 Collaborative and communicative, I excel in team environments,
+            fostering seamless coordination and collective success.
+          </li>
+          <li className="li-body">
+            Don't hesitate to take a peek at my detailed CV in PDF format. The
+            future of your projects awaits!
+          </li>
+        </ul>
+        <button className="btn-dounload">Download mi cv</button>
+      </div>
+    </div>
   )
 }
 
--- src/components/Proyecs.jsx
+++ src/components/Proyecs.jsx
@@ -1,8 +1,100 @@
-import React from 'react'
+import "./styles/proyect.css"
 
 const Proyecs = () => {
   return (
-    <div>Proyecs</div>
+    <div className="proyects-ppal-container">
+      <div className="proyects-container">
+        <div className="proyects-title">
+          <h1>Proyectos</h1>
+        </div>
+        <div className="proyects-cards-container">
+          <div className="proyects">
+            <div className="proyect">
+              <div className="proyect-img">
+                <h2 className="proyect-tittle">ejmplo1</h2>
+                <p className="p-proyects">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Debitis corporis recusandae eius! Tenetur illum minima
+                  sapiente nulla. Quaerat sint, obcaecati dicta ad minima odio
+                  saepe aut cumque, eos iusto debitis.
+                </p>
+                <img src="https://i.imgur.com/2Z3Q1ZP.png" alt="proyect" />
+              </div>
+            </div>
+          </div>
+          <div className="proyects">
+            <div className="proyect">
+              <div className="proyect-img">
+                <h2 className="proyect-tittle">ejmplo1</h2>
+                <p className="p-proyects">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Debitis corporis recusandae eius! Tenetur illum minima
+                  sapiente nulla. Quaerat sint, obcaecati dicta ad minima odio
+                  saepe aut cumque, eos iusto debitis.
+                </p>
+                <img src="https://i.imgur.com/2Z3Q1ZP.png" alt="proyect" />
+              </div>
+            </div>
+          </div>
+          <div className="proyects">
+            <div className="proyect">
+              <div className="proyect-img">
+                <h2 className="proyect-tittle">ejmplo1</h2>
+                <p className="p-proyects">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Debitis corporis recusandae eius! Tenetur illum minima
+                  sapiente nulla. Quaerat sint, obcaecati dicta ad minima odio
+                  saepe aut cumque, eos iusto debitis.
+                </p>
+                <img src="https://i.imgur.com/2Z3Q1ZP.png" alt="proyect" />
+              </div>
+            </div>
+          </div>
+          <div className="proyects">
+            <div className="proyect">
+              <div className="proyect-img">
+                <h2 className="proyect-tittle">ejmplo1</h2>
+                <p className="p-proyects">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Debitis corporis recusandae eius! Tenetur illum minima
+                  sapiente nulla. Quaerat sint, obcaecati dicta ad minima odio
+                  saepe aut cumque, eos iusto debitis.
+                </p>
+                <img src="https://i.imgur.com/2Z3Q1ZP.png" alt="proyect" />
+              </div>
+            </div>
+          </div>
+          <div className="proyects">
+            <div className="proyect">
+              <div className="proyect-img">
+                <h2 className="proyect-tittle">ejmplo1</h2>
+                <p className="p-proyects">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Debitis corporis recusandae eius! Tenetur illum minima
+                  sapiente nulla. Quaerat sint, obcaecati dicta ad minima odio
+                  saepe aut cumque, eos iusto debitis.
+                </p>
+                <img src="https://i.imgur.com/2Z3Q1ZP.png" alt="proyect" />
+              </div>
+            </div>
+          </div>
+          <div className="proyects">
+            <div className="proyect">
+              <div className="proyect-img">
+                <h2 className="proyect-tittle">ejmplo1</h2>
+                <p className="p-proyects">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Debitis corporis recusandae eius! Tenetur illum minima
+                  sapiente nulla. Quaerat sint, obcaecati dicta ad minima odio
+                  saepe aut cumque, eos iusto debitis.
+                </p>
+                <img src="https://i.imgur.com/2Z3Q1ZP.png" alt="proyect" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
